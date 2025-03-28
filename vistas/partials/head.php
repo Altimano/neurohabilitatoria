19ae86a1e07721ad31f6321d664389d44737606f
SaulGuerreroMartinez09 <136666757+SaulGuerreroMartinez09@users.noreply.github.com>
2025-03-28 15:10:55 -0600

    Prueba

--- vistas/partials/head.php
+++ vistas/partials/head.php
@@ -2,3 +2,8 @@
 <link href="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.css" rel="stylesheet" />
 <script src="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.js"></script>
 </head>
+
+<?php
+
+/*Prueba */
+?>
